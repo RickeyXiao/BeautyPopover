7060e02ac5f5b0037001e0947b5632a18c72cd22
肖伟 <1097526615@qq.com>
2017-08-27 21:51:00 +0800

    Create Project

---
+++ BeautyPopover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautyPopover
+//
+//  Created by Metallic  on 2017/8/27.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeautyPopover/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BeautyPopover
+//
+//  Created by Metallic  on 2017/8/27.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
